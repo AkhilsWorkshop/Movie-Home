eabf409d9527d785629ab8004cf71244c007bad0
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-09-23 02:23:39 -0500

    Pagination updated

--- src/components/Sub/Pagination.jsx
+++ src/components/Sub/Pagination.jsx
@@ -1,4 +1,5 @@
 import ReactPaginate from "react-paginate";
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai"
 
 const Pagination = ({ setPage, totalPages, handlePageChange }) => {
 
@@ -7,14 +8,14 @@ const Pagination = ({ setPage, totalPages, handlePageChange }) => {
             <div className="flex">
                 <ReactPaginate
                     className="flex gap-1"
-                    activeClassName="rounded border-0 bg-yellow-400 outline-none transition-all duration-300 rounded text-black hover:text-white hover:bg-white shadow-md focus:shadow-md"
+                    activeClassName="rounded border-0 bg-yellow-600 outline-none transition-all duration-300 rounded text-black hover:text-white hover:bg-white shadow-md focus:shadow-md"
                     previousClassName="relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-white hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
                     nextClassName="relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-white hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
                     pageLinkClassName="relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300 rounded text-white hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
                     previousLinkClassName=""
-                    previousLabel={'Previous'}
-                    nextLabel={'Next'}
-                    breakLabel={'...'}
+                    previousLabel={<AiFillCaretLeft size={30} />}
+                    nextLabel={<AiFillCaretRight size={30} />}
+                    breakLabel={'....'}
                     pageCount={totalPages}
                     marginPagesDisplayed={2}
                     pageRangeDisplayed={2}
