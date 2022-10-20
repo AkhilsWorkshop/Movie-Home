d8d8f3087fa53e33e4815ea4ee2d5a1404f49647
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-10-20 00:02:31 -0500

    [Fix] button error

--- src/components/PageData/HomePage/Trending.jsx
+++ src/components/PageData/HomePage/Trending.jsx
@@ -68,7 +68,7 @@ const Trending = () => {
                                 <div className="flex gap-2 flex-wrap">
                                     {/* <button className="p-2 px-3 font-semibold rounded-md w-fit bg-yellow-500 shadow-md border h-12 duration-300 text-black hover:bg-yellow-600 border-yellow-500/70 flex items-center"><BsFillPlayFill size={25} /> Watch Trailer</button> */}
                                     <Button media_type={each?.media_type} id={each?.id}>
-                                        <button className="p-2 px-3 font-semibold rounded-md bg-black/60 shadow-md border duration-200 h-12 w-28 hover:border-2 text-yellow-500 hover:border-yellow-600 border-yellow-500/70"> More Info</button>
+                                        <input type="submit" value="More Info" className="p-2 px-3 font-semibold rounded-md bg-black/60 shadow-md border duration-200 h-12 w-28 hover:border-2 hover:cursor-pointer text-yellow-500 hover:border-yellow-600 border-yellow-500/70" />
                                     </Button>
                                 </div>
 
