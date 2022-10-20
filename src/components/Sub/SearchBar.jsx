b50dcdcd99724eeb0b42b3655b140a57571e2cb0
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-10-19 23:58:08 -0500

    [Fix] searchbar bug

--- src/components/Sub/SearchBar.jsx
+++ src/components/Sub/SearchBar.jsx
@@ -29,7 +29,7 @@ const SearchBar = () => {
                     required
                 />
 
-                <span class="absolute inset-y-0 right-0 flex items-center pr-2">
+                <span className="absolute inset-y-0 right-0 flex items-center pr-2">
                     <BiSearchAlt2 onClick={getSearchQuery} size={30} className=" p-1 focus:outline-none focus:shadow-outline cursor-pointer hover:text-black" />
                 </span>
 
