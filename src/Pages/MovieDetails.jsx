31679a617fd7b8e44a91eb78a90410af4d1d0020
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-12-01 17:59:21 -0600

    [Removed] body.jsx

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -3,8 +3,17 @@ import { baseURL } from "../config/config";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loading from "../layouts/Loading";
-import Body from '../components/PageData/MovieDetails/Body';
 import ImageBg from '../components/Cards/ImageBg';
+import Trailer from "../components/PageData/MovieDetails/Trailer";
+import Title from "../components/PageData/MovieDetails/Title";
+import Poster from "../components/PageData/MovieDetails/Poster";
+import About from "../components/PageData/MovieDetails/About";
+import Stream from "../components/PageData/MovieDetails/_tests_/Stream";
+import CardSmall from "../components/Common/Cards/CardSmall";
+import Overview from "../components/PageData/MovieDetails/Overview";
+import Videos from "../components/PageData/MovieDetails/Videos";
+import Production from "../components/PageData/MovieDetails/Production";
+import ForYou from "../components/PageData/MovieDetails/_tests_/ForYou";
 
 const MovieDetails = () => {
 
@@ -22,7 +31,6 @@ const MovieDetails = () => {
         setLoading(true)
         const { data } = await axios.get(`${baseURL}/${mediaType}/${searchID}?api_key=${process.env.REACT_APP_API_KEY}&append_to_response=videos,images`);
         setData(data)
-        console.log(data, "Movie Det")
     }
 
     // Watch Providers API
@@ -56,7 +64,40 @@ const MovieDetails = () => {
 
                         <ImageBg data={data} />
 
-                        <Body data={data} credits={credits} watchProv={watchProv} mediaType={mediaType} searchID={searchID} />
+                        <div className="flex flex-col justify-center sm:px-20 xl:px-52 lg:px-32 backdrop-brightness-[100%] bg-gradient-to-b via-gray-900 from-transparent to-gray-900 text-white">
+
+                            <Trailer data={data} />
+
+                            <div className="flex flex-col gap-5 bg-gray-900 p-6 rounded-md">
+
+                                <Title data={data} />
+
+                                <div className="flex flex-col md:flex-row gap-5">
+
+                                    <div className="flex flex-col items-center gap-4">
+
+                                        <Poster data={data} stream={watchProv} />
+                                        <About data={data} mediaType={mediaType} />
+                                        <Stream stream={watchProv} />
+
+                                    </div>
+
+                                    <div className="flex flex-col w-full gap-5 overflow-hidden">
+
+                                        <CardSmall credits={credits?.cast} mediaType={mediaType} searchID={searchID} title="cast" />
+                                        <CardSmall credits={credits?.crew} mediaType={mediaType} searchID={searchID} title="crew" />
+                                        <Overview data={data} />
+                                        <Videos data={data} />
+                                        <Production companies={data?.production_companies} />
+                                        <ForYou id={data.id} mediaType={mediaType} />
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
 
                     </div>)
             }
