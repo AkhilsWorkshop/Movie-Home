5c55283e4cabb9ccafac23283da7a5d9abee4111
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-12-02 00:05:38 -0600

    [Update] Trending

--- src/components/PageData/HomePage/Trending.jsx
+++ src/components/PageData/HomePage/Trending.jsx
@@ -83,8 +83,8 @@ const Trending = () => {
                             <div className="flex flex-col gap-5 px-16 sm:px-[10rem]">
                                 <h1 className="text-2xl sm:text-[3rem] line font-bold text-blue-100 leading-tight">{each?.title || each?.name}</h1>
                                 <p className="hidden sm:block text-xl truncate">{each?.overview}</p>
-                                <div className="flex gap-2 flex-wrap">
-                                    <button onClick={() => { setIsOpen(true); getTrailer(each?.media_type, each?.id); setSlideShow(false) }} className="p-2 px-3 font-semibold rounded-md w-fit bg-yellow-500 shadow-md border h-12 duration-300 text-black hover:bg-yellow-600 border-yellow-500/70 flex items-center"><BsFillPlayFill size={25} /> Watch Trailer</button>
+                                <div className="flex gap-2">
+                                    <button onClick={() => { setIsOpen(true); getTrailer(each?.media_type, each?.id); setSlideShow(false) }} className="p-2 px-3 font-semibold rounded-md w-fit bg-yellow-500 shadow-md border h-12 duration-300 text-black hover:bg-yellow-600 border-yellow-500/70 flex items-center"><BsFillPlayFill size={25} /> Trailer</button>
                                     <Button media_type={each?.media_type} id={each?.id}>
                                         <input type="submit" value="More Info" className="p-2 px-3 font-semibold rounded-md bg-black/60 shadow-md border duration-200 h-12 w-28 hover:border-2 hover:cursor-pointer text-yellow-500 hover:border-yellow-600 border-yellow-500/70" />
                                     </Button>
