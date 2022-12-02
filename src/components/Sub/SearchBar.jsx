cacada307dc503befe016fd0f0598317778f327c
Akhil Kumar Harikumar <67311174+AkhilsWorkshop@users.noreply.github.com>
2022-12-01 20:41:20 -0600

    [Update] Search bar auto complete

--- src/components/Sub/SearchBar.jsx
+++ src/components/Sub/SearchBar.jsx
@@ -1,11 +1,17 @@
 import { BiSearchAlt2 } from "react-icons/bi";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { halfSizeImg, imgNotAvailable } from "../../config/config";
+import { useEffect } from "react";
+import axios from "axios";
+import Button from "./Button";
 
 const SearchBar = () => {
 
     // useState Hooks
     const [query, setQuery] = useState("");
+    const [autocompleteData, setAutoCompleteData] = useState([])
+    const [searchState, setSearchState] = useState(false)
 
     const navigate = useNavigate();
 
@@ -17,9 +23,19 @@ const SearchBar = () => {
         setQuery(e.target.value);
     }
 
+    const autoCompleteSearch = async (query) => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`);
+        setAutoCompleteData(data);
+        console.log(autocompleteData)
+    }
+
+    useEffect(() => {
+        autoCompleteSearch(query)
+    }, [query])
+
     return (
         <div>
-            <form className="relative text-gray-600 focus-within:text-gray-400" onSubmit={getSearchQuery}>
+            {/* <form className="relative text-gray-600 focus-within:text-gray-400" onSubmit={getSearchQuery}>
 
                 <input
                     placeholder="Search movies & more"
@@ -30,11 +46,53 @@ const SearchBar = () => {
                 />
 
                 <span className="absolute inset-y-0 right-0 flex items-center pr-2">
-                    <BiSearchAlt2 onClick={getSearchQuery} size={30} className=" p-1 focus:outline-none focus:shadow-outline cursor-pointer hover:text-black" />
+                    <BiSearchAlt2 onClick={getSearchQuery} size={30} className="p-1 focus:outline-none focus:shadow-outline cursor-pointer hover:text-black" />
                 </span>
 
-            </form>
+            </form> */}
 
+            <div className="flex items-center text-gray-600 focus-within:text-gray-400">
+                <input
+                    placeholder="Search movies & more"
+                    className={`py-2 text-base text-black bg-white rounded-md pl-4 focus:outline-none focus:bg-white focus:text-gray-900 duration-300 transition-all ${searchState ? "w-64 visible" : "w-0 invisible"}`}
+                    value={query}
+                    onChange={changeHandler}
+                    on
+                    required
+                />
+
+
+                <BiSearchAlt2 onClick={() => setSearchState(!searchState)} size={30} className={`p-1 focus:outline-none focus:shadow-outline cursor-pointer ${searchState ? "" : "hover:text-white text-slate-200"}`} />
+
+            </div>
+
+            <div className="absolute mt-2 flex flex-col h-[300px] overflow-auto overflow-x-hidden w-64 rounded-md z-50">
+                {autocompleteData?.results?.length > 0 ?
+
+                    autocompleteData?.results.map((eachItem) => (
+                        <Button media_type={eachItem.media_type} id={eachItem.id} >
+                            <div className="flex gap-2 w-64 h-auto p-2 bg-[#26262c] odd:bg-[#17171b] hover:text-[#EAB308] cursor-pointer">
+
+                                {eachItem.poster_path ?
+                                    <img src={eachItem.poster_path === null ? imgNotAvailable : halfSizeImg + eachItem.poster_path} alt="Not found" className="h-14 w-10 rounded-sm" />
+                                    :
+                                    <img src={eachItem.profile_path === null ? imgNotAvailable : halfSizeImg + eachItem.profile_path} alt="Not found" className="h-14 w-10 rounded-sm" />
+                                }
+                                <div className="flex flex-col justify-center items-start">
+                                    <p className="text-white text-sm text-left">{eachItem.title || eachItem.name}</p>
+                                    <div className="flex gap-2 items-center text-slate-500 text-sm">
+                                        <p className="">{eachItem.release_date?.slice(0, 4) || eachItem.first_air_date?.slice(0, 4)}</p>
+                                        <p className="text-white">-</p>
+                                        <p className="tracking-wide capitalize">{eachItem.media_type}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </Button>
+                    ))
+                    :
+                    <></>
+                }
+            </div>
         </div>
     )
 }
